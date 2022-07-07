7d58efe984905cfe2c06e10a8504ee01584367a1
lary <laryhunter@gmail.com>
2022-07-07 23:20:24 +0300

    base controller test get/delete

--- submodules/Core/tests/Controller/BaseControllerTest.php
+++ submodules/Core/tests/Controller/BaseControllerTest.php
@@ -16,123 +16,123 @@ use Zend\Router\RouteMatch;
 class BaseControllerTest extends TestCase
 {
 
-//    public function testDispatchBadRequest()
-//    {
-//        $this->expectException(InvalidArgumentException::class);
-//
-//        $controller = $this->getMockForAbstractClass(BaseController::class);
-//
-//        $controller->dispatch(new \Zend\Stdlib\Request());
-//    }
-//
-//    public function testOnDispatchNotAllowedMethodIsEmptyAction()
-//    {
-//        $responseMock = $this->createMock(Response::class);
-//        $responseMock->expects($this->once())
-//            ->method('setStatusCode')
-//            ->with($this->equalTo(Response::STATUS_CODE_405));
-//
-//        $event = new MvcEvent();
-//        $event->setRouteMatch($this->createMock(RouteMatch::class));
-//
-//        $controller = $this->getMockBuilder(BaseController::class)
-//            ->onlyMethods(['getResponse'])
-//            ->getMockForAbstractClass(BaseController::class);
-//
-//        $controller->method('getResponse')->willReturn($responseMock);
-//
-//        $controller->onDispatch($event);
-//    }
-//
-//    public function testOnDispatchNotAllowedMethodIsActionNotFound()
-//    {
-//        $routeMatchMock = $this->createMock(RouteMatch::class);
-//        $routeMatchMock->method('getParam')
-//            ->with($this->equalTo('action'))
-//            ->will($this->returnValue('test'));
-//
-//        $responseMock = $this->createMock(Response::class);
-//        $responseMock->expects($this->once())
-//            ->method('setStatusCode')
-//            ->with($this->equalTo(Response::STATUS_CODE_405));
-//
-//        $event = new MvcEvent();
-//        $event->setRouteMatch($routeMatchMock);
-//
-//        $controller = $this->getMockBuilder(BaseController::class)
-//            ->onlyMethods(['getResponse'])
-//            ->getMockForAbstractClass(BaseController::class);
-//
-//        $controller->method('getResponse')->willReturn($responseMock);
-//
-//        $controller->onDispatch($event);
-//    }
-//
-//    public function testOnDispatchCallMethod()
-//    {
-//        $routeMatch = $this->createMock(RouteMatch::class);
-//        $routeMatch->expects($this->once())
-//            ->method('getParam')
-//            ->with($this->equalTo('action'))
-//            ->will($this->returnValue('test'));
-//
-//        $event = new MvcEvent();
-//        $event->setRouteMatch($routeMatch);
-//
-//        $controller = $this->getMockBuilder(BaseController::class)
-//            ->addMethods(['test'])
-//            ->getMockForAbstractClass();
-//
-//        $controller->expects($this->once())
-//            ->method('test');
-//
-//        $controller->onDispatch($event);
-//    }
-//
-//    public function testOnDispatchCallMethodWithArguments()
-//    {
-//        $routeMatchMock = $this->createMock(RouteMatch::class);
-//        $routeMatchMock->expects($this->any())
-//            ->method('getParam')
-//            ->will($this->returnValueMap([
-//                ['action', null, 'test'],
-//                ['id', null,  1],
-//                ['name', null,  'some string']
-//            ]));
-//
-//        $event = new MvcEvent();
-//        $event->setRouteMatch($routeMatchMock);
-//
-//        $controller = $this->getMockBuilder(MockController::class)
-//            ->onlyMethods(['test'])
-//            ->getMock();
-//
-//        $controller->expects($this->once())
-//            ->method('test')
-//            ->with(1, 'some string');
-//
-//        $controller->onDispatch($event);
-//    }
-//
-//    public function testOnDispatchCallMethodWithNonExistArgument()
-//    {
-//        $this->expectException(ArgumentCountError::class);
-//
-//        $routeMatchMock = $this->createMock(RouteMatch::class);
-//        $routeMatchMock->method('getParam')
-//            ->will($this->returnValueMap([
-//                ['action', null, 'test']
-//            ]));
-//
-//        $event = new MvcEvent();
-//        $event->setRouteMatch($routeMatchMock);
-//
-//        $controller = $this->getMockBuilder(MockController::class)
-//            ->onlyMethods(['test'])
-//            ->getMock();
-//
-//        $controller->onDispatch($event);
-//    }
+    public function testDispatchBadRequest()
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        $controller = $this->getMockForAbstractClass(BaseController::class);
+
+        $controller->dispatch(new \Zend\Stdlib\Request());
+    }
+
+    public function testOnDispatchNotAllowedMethodIsEmptyAction()
+    {
+        $responseMock = $this->createMock(Response::class);
+        $responseMock->expects($this->once())
+            ->method('setStatusCode')
+            ->with($this->equalTo(Response::STATUS_CODE_405));
+
+        $event = new MvcEvent();
+        $event->setRouteMatch($this->createMock(RouteMatch::class));
+
+        $controller = $this->getMockBuilder(BaseController::class)
+            ->onlyMethods(['getResponse'])
+            ->getMockForAbstractClass(BaseController::class);
+
+        $controller->method('getResponse')->willReturn($responseMock);
+
+        $controller->onDispatch($event);
+    }
+
+    public function testOnDispatchNotAllowedMethodIsActionNotFound()
+    {
+        $routeMatchMock = $this->createMock(RouteMatch::class);
+        $routeMatchMock->method('getParam')
+            ->with($this->equalTo('action'))
+            ->will($this->returnValue('test'));
+
+        $responseMock = $this->createMock(Response::class);
+        $responseMock->expects($this->once())
+            ->method('setStatusCode')
+            ->with($this->equalTo(Response::STATUS_CODE_405));
+
+        $event = new MvcEvent();
+        $event->setRouteMatch($routeMatchMock);
+
+        $controller = $this->getMockBuilder(BaseController::class)
+            ->onlyMethods(['getResponse'])
+            ->getMockForAbstractClass(BaseController::class);
+
+        $controller->method('getResponse')->willReturn($responseMock);
+
+        $controller->onDispatch($event);
+    }
+
+    public function testOnDispatchCallMethod()
+    {
+        $routeMatch = $this->createMock(RouteMatch::class);
+        $routeMatch->expects($this->once())
+            ->method('getParam')
+            ->with($this->equalTo('action'))
+            ->will($this->returnValue('test'));
+
+        $event = new MvcEvent();
+        $event->setRouteMatch($routeMatch);
+
+        $controller = $this->getMockBuilder(BaseController::class)
+            ->addMethods(['test'])
+            ->getMockForAbstractClass();
+
+        $controller->expects($this->once())
+            ->method('test');
+
+        $controller->onDispatch($event);
+    }
+
+    public function testOnDispatchCallMethodWithArguments()
+    {
+        $routeMatchMock = $this->createMock(RouteMatch::class);
+        $routeMatchMock->expects($this->any())
+            ->method('getParam')
+            ->will($this->returnValueMap([
+                ['action', null, 'test'],
+                ['id', null,  1],
+                ['name', null,  'some string']
+            ]));
+
+        $event = new MvcEvent();
+        $event->setRouteMatch($routeMatchMock);
+
+        $controller = $this->getMockBuilder(MockController::class)
+            ->onlyMethods(['test'])
+            ->getMock();
+
+        $controller->expects($this->once())
+            ->method('test')
+            ->with(1, 'some string');
+
+        $controller->onDispatch($event);
+    }
+
+    public function testOnDispatchCallMethodWithNonExistArgument()
+    {
+        $this->expectException(ArgumentCountError::class);
+
+        $routeMatchMock = $this->createMock(RouteMatch::class);
+        $routeMatchMock->method('getParam')
+            ->will($this->returnValueMap([
+                ['action', null, 'test']
+            ]));
+
+        $event = new MvcEvent();
+        $event->setRouteMatch($routeMatchMock);
+
+        $controller = $this->getMockBuilder(MockController::class)
+            ->onlyMethods(['test'])
+            ->getMock();
+
+        $controller->onDispatch($event);
+    }
 
     /**
      * @dataProvider onDispatchCallGetAndDeleteMethodsProvider
